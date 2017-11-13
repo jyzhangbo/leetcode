28a0222d5057dfc5cfa4b27edde8e2026da9b501
zhangboad <zhangboad@enn.cn>
2017-11-13 17:30:04 +0800

    add ReverseInteger

--- src/cn/test/LongestPalindromic.java
+++ src/cn/test/LongestPalindromic.java
@@ -7,7 +7,7 @@ package cn.test;
 public class LongestPalindromic {
 
   public static void main(String[] args) {
-    System.out.println(longestPalindrome1("ababd"));
+    System.out.println(longestPalindrome1("aaabbaaa"));
   }
 
 
